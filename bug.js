0b84ad4794ba5f691c6705a43d792fa0a67ed007
BugHunter-X <askzenai@gmail.com>
2024-12-13 12:02:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,21 @@
+```javascript
+import React, { useState, useEffect } from 'react';
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This will cause a warning in React 18 and later
+    console.log('count:', count); //this will log the value of count in every render
+  }, [count]);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
